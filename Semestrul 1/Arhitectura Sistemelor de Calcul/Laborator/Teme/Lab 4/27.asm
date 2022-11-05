461015f756139a3f0d1cdb0caad277f1d694c728
RegusAl <regusal@pm.me>
2022-11-05 20:31:11 +0200

    ASC_hw_lab4

--- Semestrul 1/Arhitectura Sistemelor de Calcul/Laborator/Teme/Lab 4/27.asm
+++ Semestrul 1/Arhitectura Sistemelor de Calcul/Laborator/Teme/Lab 4/27.asm
@@ -30,23 +30,22 @@ segment code use32 class=code
        mov edx, [a+4]               ; edx:eax = a
        
        and edx, 000000069h
-       and dl, 0001_1100b
-       mov cl, 2
-       ror dl, cl                           ; dl  = 0000_0010b
+       and dl, 0011_1000b
+       mov cl, 3
+       ror dl, cl                           ; dl  = 0000_0101b
        mov [n], dl
       
        mov cl, [n]
        ror eax, cl                  ; se roteste la dreapta doubleword-ul inferior (eax) a lui a cu n pozitii
        mov [b], eax
 
-       and ax, 0000_1111_0000_0000b   ; izolam bitii 9-11 ai lui b
+       and ax, 0000_1111_0000_0000b   ; izolam bitii 8-11 ai lui b
        mov cl, 8
        ror ax, cl
        or bx, ax  
        
        mov ax, [b+2]
        and al, 0000_1111b    ; izolam bitii 16-19 ai lui b
-       mov ah, 0
        mov cl, 4
        rol al, cl          
        or bx, ax

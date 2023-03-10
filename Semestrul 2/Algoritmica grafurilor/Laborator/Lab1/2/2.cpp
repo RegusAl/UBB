c08fa07ee1c72526a7dad60bec9444c2ae57c079
RegusAl <regusal@pm.me>
2023-03-10 11:41:14 +0200

    AG/lab1

--- Semestrul 2/Algoritmica grafurilor/Laborator/Lab1/2/2.cpp
+++ Semestrul 2/Algoritmica grafurilor/Laborator/Lab1/2/2.cpp
@@ -1,15 +1,14 @@
 #include <iostream>
 #include <fstream>
-
+#define inf 10000000
 using  namespace std;
 
-ifstream fin("/home/albert/Documents/UBB/Semestrul 2/Algoritmica grafurilor/Laborator/Lab1/1/fisier.in");
+ifstream fin("fisier.in");
 
-int main() {
+int n, mat[10][10] = {0}, d[10][10] = {0};
+int viz[100] = {0};
 
-    int n; // numarul de noduri
-    fin >> n;
-    int mat[10][10] = {0};
+void lista_muchii() {
     int i, j;
     cout<<"Lista de muchii: "<<endl;
     cout<<n<<endl;
@@ -18,16 +17,18 @@ int main() {
         mat[i][j] = 1;
         mat[j][i] = 1;
     }
+}
 
-    cout<<"Matricea de adiacenta: "<<endl;
+void matrice_adiacenta() {
     for(int i = 1; i<=n; i++) {
         for (int j = 1; j<=n; j++) {
             cout<<mat[i][j]<<" ";
         }
         cout<<"\n";
     }
+}
 
-    // noduri izolate
+void noduri_izolate() {
     int nr = 0;
     for(int i = 1; i<=n; i++) {
         int ok = 0;
@@ -44,9 +45,9 @@ int main() {
     if(nr==0)
         cout<<"Nu exista noduri izolate!"<<endl;
 
+}
 
-
-    // determinare graf regular
+int graf_regular() {
     int v[10] = {0};
     for(int i = 1; i<=n; i++) {
         for(int j = 1; j<=n; j++) {
@@ -55,49 +56,110 @@ int main() {
             }
         }
     }
-    int ok = 1;
-    for(i = 1; i < n; i++) {
+    for(int i = 1; i < n; i++) {
         if(v[i]!=v[i+1]) {
-            ok = 0;
+            return 0;
         }
     }
-    if(ok==0) {
+    return 1;
+}
+
+void matricea_distantelor() {
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j<=n; j++) {
+            if(mat[i][j]==1)
+                d[i][j] = 1;
+            else
+                d[i][j] = inf;
+        }
+    }
+}
+
+void roy_floyd() {
+    for(int k = 1; k<=n; k++) {
+        for(int i = 1; i<=n; i++) {
+            for(int j = 1; j<=n; j++) {
+                if(d[i][k]!=inf && d[k][j]!=inf) {
+                    if(d[i][j] > d[i][k] + d[k][j]) {
+                        d[i][j] = d[i][k] + d[k][j];
+                    }
+                }
+            }
+        }
+    }
+}
+
+void afisare_matricea_distantelor() {
+    for(int i = 1; i<=n; i++) {
+        d[i][i] = 0;
+        for(int j = 1; j<=n; j++) {
+            if(d[i][j] == inf)
+                cout<<"inf"<<" ";
+            else
+                cout<<d[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+void DFS(int x) {
+    int i;
+    viz[x] = 1;
+    for(i = 1; i<=n; i++) {
+        if(mat[x][i]==1 && viz[i]==0)
+            DFS(i);
+    }
+}
+
+int conex(int n) {
+    DFS(1);
+    for(int i = 1; i<=n; i++) {
+        if(viz[i]==0)
+            return 0;
+    }
+    return 1;
+}
+
+int main() {
+
+    fin >> n;
+
+    int i, j;
+    lista_muchii();
+    cout<<"----------"<<endl;
+
+    cout<<"Matricea de adiacenta: "<<endl;
+    matrice_adiacenta();
+    cout<<"----------"<<endl;
+
+    // noduri izolate
+    noduri_izolate();
+    cout<<"----------"<<endl;
+
+    // determinare graf regular
+    if(graf_regular()==0) {
         cout<<"Graful nu este regular!"<<endl;
     } else {
         cout<<"Graful este regular!"<<endl;
     }
+    cout<<"----------"<<endl;
 
-//    int **drumuri = new int*[n];
-//    for (int i = 1; i <= n; i++) {
-//        drumuri[i] = new int[n];
-//        for (int j = 1; j <= n; j++) {
-//            if(mat[i][j] != 1) {
-//                drumuri[i][j] = -1;
-//            } else {
-//                drumuri[i][j] = mat[i][j];
-//            }
-//
-//        }
-//    }
-//
-//    for (int k = 1; k <= n; k++) {
-//        for (int i = 1; i <= n; i++) {
-//            for (int j = 1; j <= n; j++) {
-//                if (drumuri[i][k]==-1 && drumuri[k][j]==-1) {
-//                    if(drumuri[i][j] > drumuri[i][k] + drumuri[k][j])
-//                        drumuri[i][j] += drumuri[i][k] + drumuri[k][j];
-//                }
-//            }
-//        }
-//    }
-//
-//    cout << "Matricea drumurilor este: " << endl;
-//    for (int i = 1; i <= n; i++) {
-//        for (int j = 1; j <= n; j++) {
-//            cout << drumuri[i][j] << " ";
-//        }
-//        cout << endl;
-//    }
+    // Matricea distantelor
+    matricea_distantelor();
+
+    // roy-floyd
+    roy_floyd();
+
+    // afisare matricea drumurilor
+    afisare_matricea_distantelor();
+    cout<<"----------"<<endl;
+
+    if(conex(n)) {
+        cout<<"Graful este conex."<<endl;
+    } else {
+        cout<<"Graful nu este conex."<<endl;
+    }
+    cout<<"----------"<<endl;
 
     fin.close();
 

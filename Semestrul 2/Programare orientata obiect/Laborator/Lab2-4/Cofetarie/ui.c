c40d30160eadf7f020f1b2d7f48f2a4368bb7336
RegusAl <regusal@pm.me>
2023-03-11 14:43:10 +0200

    oop/lab2

--- Semestrul 2/Programare orientata obiect/Laborator/Lab2-4/Cofetarie/ui.c
+++ Semestrul 2/Programare orientata obiect/Laborator/Lab2-4/Cofetarie/ui.c
@@ -103,6 +103,29 @@ int stergeMateriePrima(List *v, char *nume, char *producator) {
     return 0;
 }
 
+int modificaMateriePrima(List *v, char *nume, char *producator, char *nume_nou, char *producator_nou, int cantitate) {
+    int sters = stergeMateriePrima(v, nume, producator);
+    if(sters==0) {
+        return 0;
+    } else {
+        int adauga = adaugaMateriePrima(v, nume_nou, producator_nou, cantitate);
+        if(adauga==0)
+            return 0;
+    }
+    return 1;
+}
+
+List filtruMateriePrima(List *v, char l, int cantitate_max) {
+    List listaFiltrata = createEmpty();
+    for(int i = 0; i< size(v); i++) {
+        MateriePrima m = get(v, i);
+        if(m.nume[0] == l && m.cantitate < cantitate_max) {
+            add(&listaFiltrata, creeazaMateriePrima(m.nume, m.producator, m.cantitate));
+        }
+    }
+    return listaFiltrata;
+}
+
 // ui
 void uiAdd(List *v) {
     char nume[30], producator[30];
@@ -118,6 +141,27 @@ void uiAdd(List *v) {
         printf("Materie Prima adaugata cu succes!\n");
     else
         printf("Materia Prima nu este valida!\n");
+}
+
+void uiModify(List *v) {
+    char nume[30], nume_nou[30], producator[30], producator_nou[30];
+    int cantitate;
+    printf("De modificat\n");
+    printf("Nume: ");
+    scanf("%s", nume);
+    printf("Producator: ");
+    scanf("%s", producator);
+    printf("Nume nou: ");
+    scanf("%s", nume_nou);
+    printf("Producator nou: ");
+    scanf("%s", producator_nou);
+    printf("Cantitate: ");
+    scanf("%d", &cantitate);
+    int succes = modificaMateriePrima(v, nume, producator, nume_nou, producator_nou, cantitate);
+    if(succes)
+        printf("Modificarea s-a efectuat cu succes!\n");
+    else
+        printf("Modificarea nu s-a efectuat! Date invalide!\n");
 }
 
 void uiDelete(List *v) {
@@ -145,12 +189,25 @@ void uiStoc(List *v) {
     }
 }
 
+void uiAfisareLitera(List *v) {
+    char l;
+    int cantitate_max;
+    printf("Prima litera: ");
+    scanf(" %c", &l);
+    printf("Cantitatea maxima: ");
+    scanf("%d", &cantitate_max);
+    List listaFiltrata = filtruMateriePrima(v, l, cantitate_max);
+    uiStoc(&listaFiltrata);
+}
+
 void uiMenu() {
     printf("\t\t--MENIU--\n");
     printf("1. Adaugare Materie Prima (daca exista deja in stoc se actualizeaza cantitatea\n");
     printf("2. Modificare Materie Prima\n");
     printf("3. Stergere Materie Prima\n");
     printf("4. Afisare Stoc\n");
+    printf("5. Afisare materii prime ale carui nume incepe cu o litera data de utilizator si au cantitatea mai mica decat cea data de utilizator\n");
+    printf("6. Afisare stoc sortat dupa nume si cantitate\n");
     printf("0. Iesire din program.\n");
     printf("--\t--\t--\n");
 }
@@ -169,6 +226,7 @@ void run() {
                 break;
             case 2:
                 printf("2\n");
+                uiModify(&listaMateriePrima);
                 break;
             case 3:
                 printf("3\n");
@@ -177,6 +235,10 @@ void run() {
             case 4:
                 printf("4\n");
                 uiStoc(&listaMateriePrima);
+                break;
+            case 5:
+                printf("5\n");
+                uiAfisareLitera(&listaMateriePrima);
                 break;
             case 0:
                 printf("Iesire din program.\n");

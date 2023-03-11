f2569b1c5ac95b13381733ab48812fa5522a6cc1
RegusAl <regusal@pm.me>
2023-03-11 13:12:05 +0200

    oop/lab2

---
+++ Semestrul 2/Programare orientata obiect/Laborator/Lab2-4/Cofetarie/ui.c
@@ -0,0 +1,195 @@
+#include <stdio.h>
+#include <string.h>
+
+// domain
+typedef struct {
+    char nume[30];
+    char producator[30];
+    int cantitate;
+} MateriePrima;
+
+MateriePrima creeazaMateriePrima(char *nume, char *producator, int cantitate) {
+    MateriePrima m;
+    strcpy(m.nume, nume);
+    strcpy(m.producator, producator);
+
+    m.cantitate = cantitate;
+    return m;
+}
+
+int valideazaMateriePrima(MateriePrima m) {
+    if(strlen(m.nume) == 0)
+        return 0;
+    if(strlen(m.producator) == 0)
+        return 0;
+    if(m.cantitate < 0)
+        return 0;
+    return 1;
+}
+
+void destroyMateriePrima(MateriePrima *m) {
+    m -> nume[0] = '\0';
+    m -> producator[0] = '\0';
+    m ->cantitate = -1;
+}
+
+// list
+typedef MateriePrima ElemType;
+typedef struct {
+    ElemType elements[100];
+    int length;
+} List;
+
+List createEmpty() {
+    List v;
+    v.length = 0;
+    return v;
+}
+
+void add(List *v, ElemType el) {
+    v->elements[v->length] = el;
+    v->length++;
+}
+
+void addCantitate(List *v, ElemType el, int poz) {
+    v->elements[poz] = el;
+}
+
+ElemType delete(List *v, int poz) {
+    ElemType el = v->elements[poz];
+    for(int i = poz; i<v->length-1; i++) {
+        v->elements[i] = v->elements[i+1];
+    }
+    v->length--;
+    return el;
+}
+
+ElemType get(List *v, int poz) {
+    return v->elements[poz];
+}
+
+int size(List *v) {
+    return v->length;
+}
+
+// service
+int adaugaMateriePrima(List *v, char *nume, char *producator, int cantitate) {
+    MateriePrima m = creeazaMateriePrima(nume, producator, cantitate);
+    if(valideazaMateriePrima(m)==0)
+        return 0;
+    int ok = 0;
+    // se modifica cantitatea daca se da o materie prima cu acelasi nume si producator ce este in stoc.
+    for(int i = 0; i< size(v); i++) {
+        if(strcmp(get(v, i).nume, m.nume)==0 && strcmp(get(v, i).producator, m.producator)==0) {
+            addCantitate(v, m, i);
+            ok = 1;
+        }
+    }
+    if(ok == 0)
+        add(v, m);
+    return 1;
+}
+
+int stergeMateriePrima(List *v, char *nume, char *producator) {
+    int poz = -1;
+    for(int i = 0; i< size(v); i++) {
+        if(strcmp(get(v, i).nume, nume)==0 && strcmp(get(v, i).producator, producator)==0) {
+            poz = i;
+            MateriePrima m = delete(v, poz);
+            destroyMateriePrima(&m);
+            return 1;
+        }
+    }
+    return 0;
+}
+
+// ui
+void uiAdd(List *v) {
+    char nume[30], producator[30];
+    int cantitate;
+    printf("Nume: ");
+    scanf("%s", nume);
+    printf("Producator: ");
+    scanf("%s", producator);
+    printf("Cantitate: ");
+    scanf("%d", &cantitate);
+    int succes = adaugaMateriePrima(v, nume, producator, cantitate);
+    if(succes)
+        printf("Materie Prima adaugata cu succes!\n");
+    else
+        printf("Materia Prima nu este valida!\n");
+}
+
+void uiDelete(List *v) {
+    char nume[30], producator[30];
+    printf("Numele Materiei Prime de sters: ");
+    scanf("%s", nume);
+    printf("Numele producatorului de sters: ");
+    scanf("%s", producator);
+    int succes = stergeMateriePrima(v, nume, producator);
+    if(succes)
+        printf("Materie Prima stearsa cu succes!\n");
+    else
+        printf("Nu exista Materie Prima de acest fel!\n");
+}
+
+void uiStoc(List *v) {
+    if(v->length == 0)
+        printf("Nu este nimic in stoc!\n");
+    else {
+        printf("Stoc: \n");
+        for(int i = 0; i<size(v); i++) {
+            MateriePrima m = get(v, i);
+            printf("Nume: %s | Producator: %s | Cantitate: %d\n", m.nume, m.producator, m.cantitate);
+        }
+    }
+}
+
+void uiMenu() {
+    printf("\t\t--MENIU--\n");
+    printf("1. Adaugare Materie Prima (daca exista deja in stoc se actualizeaza cantitatea\n");
+    printf("2. Modificare Materie Prima\n");
+    printf("3. Stergere Materie Prima\n");
+    printf("4. Afisare Stoc\n");
+    printf("0. Iesire din program.\n");
+    printf("--\t--\t--\n");
+}
+
+void run() {
+    List listaMateriePrima = createEmpty();
+    int ok = 1, option;
+    while(ok) {
+        uiMenu();
+        printf("Optiune: ");
+        scanf("%d", &option);
+        switch (option) {
+            case 1:
+                printf("1\n");
+                uiAdd(&listaMateriePrima);
+                break;
+            case 2:
+                printf("2\n");
+                break;
+            case 3:
+                printf("3\n");
+                uiDelete(&listaMateriePrima);
+                break;
+            case 4:
+                printf("4\n");
+                uiStoc(&listaMateriePrima);
+                break;
+            case 0:
+                printf("Iesire din program.\n");
+                ok = 0;
+                break;
+            default:
+                printf("Optiunea nu este valida!\n");
+                break;
+        }
+    }
+}
+
+int main() {
+    run();
+    return 0;
+}

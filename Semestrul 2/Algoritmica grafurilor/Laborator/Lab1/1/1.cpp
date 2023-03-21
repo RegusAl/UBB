ce262b9ef10ae90c09cf29cc09471a1af5d8be80
RegusAl <regusal@pm.me>
2023-03-21 19:09:02 +0200

    ag/lab

--- Semestrul 2/Algoritmica grafurilor/Laborator/Lab1/1/1.cpp
+++ Semestrul 2/Algoritmica grafurilor/Laborator/Lab1/1/1.cpp
@@ -1,33 +1,33 @@
-//#include <iostream>
-//#include <fstream>
-//
-//using  namespace std;
-//
-//ifstream fin("/home/albert/Documents/UBB/Semestrul 2/Algoritmica grafurilor/Laborator/Lab1/1/fisier.in");
-//
-//int main() {
-//
-//    int n; // numarul de noduri
-//    fin >> n;
-//    int mat[10][10] = {0};
-//    int i, j;
-//    cout<<"Lista de muchii: "<<endl;
-//    cout<<n<<endl;
-//    while(fin>>i>>j) {
-//        cout << i << " " << j << endl;
-//        mat[i][j] = 1;
-//        mat[j][i] = 1;
-//    }
-//
-//    cout<<"Matricea de adiacenta: "<<endl;
-//    for(int i = 1; i<=n; i++) {
-//        for (int j = 1; j<=n; j++) {
-//            cout<<mat[i][j]<<" ";
-//        }
-//        cout<<"\n";
-//    }
-//
-//    fin.close();
-//
-//    return 0;
-//}
+#include <iostream>
+#include <fstream>
+
+using  namespace std;
+
+ifstream fin("fisier.in");
+
+int main() {
+
+    int n; // numarul de noduri
+    fin >> n;
+    int mat[10][10] = {0};
+    int i, j;
+    cout<<"Lista de muchii: "<<endl;
+    cout<<n<<endl;
+    while(fin>>i>>j) {
+        cout << i << " " << j << endl;
+        mat[i][j] = 1;
+        mat[j][i] = 1;
+    }
+
+    cout<<"Matricea de adiacenta: "<<endl;
+    for(int i = 1; i<=n; i++) {
+        for (int j = 1; j<=n; j++) {
+            cout<<mat[i][j]<<" ";
+        }
+        cout<<"\n";
+    }
+
+    fin.close();
+
+    return 0;
+}
---
+++ Semestrul 2/Algoritmica grafurilor/Laborator/Lab2/2/2.cpp
@@ -0,0 +1,3 @@
+//
+// Created by albert on 21/03/23.
+//

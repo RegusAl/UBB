6d63f794c9688448fa8dc7a3ddb722df379147d8
RegusAl <regusal@pm.me>
2023-05-31 15:58:52 +0300

    ag

---
+++ Semestrul 2/Algoritmica grafurilor/Laborator/Lab4/P2/p2.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+ifstream fin("/home/albert/Documents/UBB/Semestrul 2/Algoritmica grafurilor/Laborator/Lab4/P1/fisier.in");
+int parinte[101], pruffer[101];
+
+// gasim frunza minima
+//int frunzaMinima(int n) {
+//    int fm = 0, i = 0;
+//    while (i < n) {
+//        if (fm == parinte[i]) {
+//            fm++;
+//            i = -1;
+//        }
+//        i++;
+//    }
+//    return fm;
+//}
+
+//void codarePrufer(int n) {
+//    for(int i = 0; i<n-1; i++) {
+//        int fm = frunzaMinima(n);
+//        pruffer[i] = parinte[fm];
+//        parinte[fm] = fm;
+//    }
+//}
+
+int main() {
+    int n;
+    fin>>n;
+    for(int i = 0; i<n; i++) {
+        fin>>parinte[i];
+    }
+//    codarePrufer(n);
+    // afisare
+    cout<<n-1<<endl;
+    for(int i = 0; i<n-1; i++) {
+        cout<<pruffer[i]<<" ";
+    }
+
+    return 0;
+}
